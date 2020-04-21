3a35f852f810953fa6c2204026a92932f4b431f0
yonko <i.bellaouedj@esi-sba.dz>
2020-04-21 02:22:24 +0100

    Ad3iya.ejs,Header.ejs,manasik-omra.ejs => edited;Add Contact

--- config/api.js
+++ config/api.js
@@ -10,6 +10,7 @@ let router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
+
 //-------------(Send mail:forgot password)-------------
 //you get your SENDGRID_API_KEY when u create new AÏ_KEY in Send Grid
 //you had to add SENDGRID_API_KEY in your variabales environnemet
@@ -24,19 +25,29 @@ var msg = {
 };
 router.post('/forgotPSW', (request, response) => {
   var mail=request.body
-  sgMail.send(msg={
-    to: mail.forgot,
-    from: 'i.bellaouedj@esi-sba.dz',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'Hello m Yonko i m glad you read that ',
-    html: '<strong>Hello m Yonko i m glad you read that</strong>',
-  }).then(() => {
-      console.log('Message sent')
-  }).catch((error) => {
-      console.log(error.response.body)
-      // console.log(error.response.body.errors[0].message)
-  })
-  response.end()
+  var sql =" SELECT * from users WHERE UserMail = ?";
+  mysqlConnection.query(sql, [mail.forgot],(err,rows,fields)=>{
+    if (!err) {
+        console.log(rows[0].UserPassword);
+        sgMail.send(msg={
+           to: mail.forgot,
+           from: 'i.bellaouedj@esi-sba.dz',
+           subject: 'Sending with Twilio SendGrid is Fun',
+           text: 'Hello m Yonko i m glad you read that this is ur password ' + rows[0].UserPassword,
+           html: '<strong>Hello m Yonko i m glad you read that this is ur password :</strong> ' + rows[0].UserPassword,
+         }).then(() => {
+              console.log('Message sent')
+         }).catch((error) => {
+            console.log(error.response.body)
+           // console.log(error.response.body.errors[0].message)
+        })
+        response.render('Home page/forgot-pass')
+        response.end()
+  }
+  else {
+    console.log(err);
+  }
+});
 })
 //------------------------------------------------------
 
